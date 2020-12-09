37cb45937a15f09d3111bb77602a2f90de3032c4
Urvashi <Urvashimalik867@gmail.com>
2020-12-10 00:34:03 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var userText = document.querySelector("#userInput");
+var translateBtn = document.querySelector("#btn-translator");
+var sampleBtn = document.querySelector("#btn-sample");
+var refreshBtn = document.querySelector("#btn-refresh");
+var emoticonOp = document.querySelector("#emojiOutput");
+
+var serverURL = "https://api.funtranslations.com/translate/emoji.json";
+
+function constructURL(text){
+    var finalURL = serverURL + "?" + "text=" + text;
+    return finalURL;
+}
+
+function errorHandler(error) {
+    console.log("Error Occured: "+error);
+    alert("Maximun count is excedeed. Please try after an hour.");
+}
+
+function clickHandler() {
+    var enterdText = userText.value;
+    fetch(constructURL(enterdText))
+    .then(response => response.json())
+    .then(json => {emoticonOp.innerText = json.contents.translated})
+    .catch(errorHandler)
+}
+
+translateBtn.addEventListener("click", clickHandler);
+
+sampleBtn.addEventListener("click", () => {
+    userText.value = "Apple is great Fruit";
+})
+
+refreshBtn.addEventListener("click", () => {
+    userText.value = "";
+    emoticonOp.innerText= "";
+})
